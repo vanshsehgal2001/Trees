e1e0357ac0fac8fad8ed6174b12dee9cd40dfb25
vanshsehgal2001 <62723522+vanshsehgal2001@users.noreply.github.com>
2021-02-24 21:50:19 +0530

    Create MaxDataNode.cpp

---
+++ MaxDataNode.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+template<typename T>
+
+class TreeNode{
+    public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data){
+        this->data=data;
+    }
+};
+
+TreeNode<int>* inputLevelWise(){
+    int rootdata;
+    cin>>rootdata;
+    TreeNode<int>* root=new TreeNode<int>(rootdata);
+    queue<TreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()){
+        TreeNode<int>* front=q.front();
+        q.pop();
+        int num;
+        cout<<"Enter no of children of "<<front->data<<" ";
+        cin>>num;
+        for(int i=0;i<num;i++){
+            int data;
+            cout<<"Enter the data of "<<i<<" child";
+            cin>>data;
+            TreeNode<int>* child=new TreeNode<int>(data);
+            front->children.push_back(child);
+            q.push(child);
+        }
+    }
+    return root;
+}
+
+int maxDataNode(TreeNode<int>* root){
+    int ans=root->data;
+    for(int i=0;i<root->children.size();i++){
+        ans=max(ans,maxDataNode(root->children[i]));
+    }
+    return ans;
+}
+
+int main()
+{
+    TreeNode<int> *root=inputLevelWise();
+    cout<<maxDataNode(root);
+    return 0;
+}
